8ccbbc181244b9f3ff15feb641db210c4772a533
koni123 <niko.tastula@gmail.com>
2024-03-12 20:00:49 +0200

    clean up

--- IntegrationTest/DatabaseService/TestWebApplicationFactory.cs
+++ IntegrationTest/DatabaseService/TestWebApplicationFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Common;
-using DatabaseService;
+﻿using DatabaseService;
 using DatabaseService.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
